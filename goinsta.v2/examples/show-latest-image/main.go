9b3e8b10604acdeee759ca863e8f6661ad54ee6f
Jesus-Sheriff <apuntabienmicorreo@correo.ugr.es>
2019-10-07 11:14:28 +0200

    Añadido Travis y codigo fuente funcionando

---
+++ goinsta.v2/examples/show-latest-image/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/ahmdrz/goinsta.v2"
+)
+
+func main() {
+	// insta := goinsta.New(
+	// 	os.Getenv("INSTAGRAM_USERNAME"),
+	// 	os.Getenv("INSTAGRAM_PASSWORD"),
+	// )
+
+	//testing!!!!
+
+	insta := goinsta.New(
+		"apuntabienminombre",
+		"77142837Dd",
+	)
+	if err := insta.Login(); err != nil {
+		log.Println(err)
+		return
+	}
+	defer insta.Logout()
+
+	feedTag, err := insta.Feed.Tags("golang")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//log.Printf("ultima foto: ", feedTag.Images[0].User.Username)
+	log.Printf("ultima foto:  %s", feedTag.Images[0].Images.GetBest())
+	// for _, item := range feedTag.RankedItems {
+	// 	user := item.User
+	// 	user.SetInstagram(insta)
+
+	// 	err = user.Follow()
+	// 	if err != nil {
+	// 		log.Printf("error on following user %s, %v", user.Username, err)
+	// 	} else {
+	// 		log.Printf("start following user %s", user.Username)
+	// 	}
+	// }
+}
